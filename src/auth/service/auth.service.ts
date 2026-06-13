a3bd6dd0aa2a1211182eab18d6c51b9bfab1e1e3
Lucas <lucastheodoromourofernandes@gmail.com>
2026-06-13 15:51:58 -0400

    feat : Configurações iniciais de autenticação com JWT

---
+++ src/auth/service/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  validateJwtToken() {}
+  validateSessionToken() {}
+  login() {}
+  register() {}
+}
